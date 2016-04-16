553c44a450466091d348b9745c1198e5e22d1798
T.J. Tarazevits <tarazevits@gmail.com>
2016-04-16 12:28:15 -0400

    Added data sheets for sensors and started Arduion Project

---
+++ MainBoard Software/mainBoardCode/mainBoardCode.ino
@@ -0,0 +1,39 @@
+//calls init
+void setup() {
+  // put your setup code here, to run once:
+
+}
+//main program loop
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//initialize state variables and sensors
+//Check SD card health
+void init() {
+
+}
+
+//starts timer and passes whatever incrementor clock uses
+void startTimer() {
+  
+}
+
+//reads data from each sensor and stores it to stringBuilder/SD
+void pollSensors() {
+  
+}
+
+//receives data from external board
+void receiveExternalBoardData() {
+  
+}
+
+//triggers the external payload deployment switch
+void deployPayload() {
+  
+}
+
+
+

dedb33a68945e6471738de35e5473575e8fb7118
T.J. Tarazevits <tarazevits@gmail.com>
2016-04-27 02:44:47 -0400

    Added Mainboard Software v0.1
    -Successfully blinks heartbeat
    -raises Relay 17 to HIGH after 15s

--- MainBoard Software/mainBoardCode/mainBoardCode.ino
+++ MainBoard Software/mainBoardCode/mainBoardCode.ino
@@ -1,18 +1,74 @@
+#include <SPI.h>
+#include <SD.h>
+#include <elapsedMillis.h>
+#include <Wire.h>
+#include <ADXL345.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+elapsedMillis ledFlash, pollSensor, releasePayload;
+ADXL345 accel;
+
+//SD card value
+const int sdSelect = 4;
+
+uint8_t led=14;
+uint8_t relay = 17;
+unsigned int blinkRate = 500;
+//unsigned int releaseTime = 300000; //5 minutes = 5 * 60 * 1000
+unsigned int releaseTime = 15000; //testing time of 15s
+boolean ledState = LOW;
+boolean payloadReleased = false;
+int accelAddr = 0x53;
+
+ int accelX, accelY, accelZ;
+
 //calls init
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  digitalWrite(led, ledState);
+  
+  Serial.begin(9600);
+  //Wire.begin(accelAddr);
+
+  //initialize Accelerometer
+  //setupAccel();
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, LOW);
+  
 
 }
 //main program loop
 void loop() {
-  // put your main code here, to run repeatedly:
+    flashLED();
 
+    /*if(pollSensor > 100) {
+      pollSensors();
+      pollSensor = 0;
+    }*/
+    
+    if(releasePayload>releaseTime && !payloadReleased) {
+      digitalWrite(relay, HIGH);
+      Serial.println("relay raised HIGH");
+      //releasePayload = 0;
+      payloadReleased = true;
+    }
 }
 
 //initialize state variables and sensors
 //Check SD card health
-void init() {
-
+bool init() {
+  if(SD.begin(sdSelect)) {
+       for(int i = 0; i<5; i++) {
+        /*
+        digitalWrite(led, HIGH);
+        delay(500);
+        digitalWrite(led, LOW);
+        */
+    }
+    return false;
+  }
+  return true;
 }
 
 //starts timer and passes whatever incrementor clock uses
@@ -22,7 +78,8 @@ void startTimer() {
 
 //reads data from each sensor and stores it to stringBuilder/SD
 void pollSensors() {
-  
+  accel.readAccel(&accelX,&accelY,&accelZ);
+  Serial.println((accelX + 78) - 65536);
 }
 
 //receives data from external board
@@ -33,6 +90,35 @@ void receiveExternalBoardData() {
 //triggers the external payload deployment switch
 void deployPayload() {
   
+}
+
+void setupAccel() {
+  //initialize accelerometer
+  accel.powerOn();
+  accel.setActivityThreshold(75);
+  accel.setInactivityThreshold(300);
+  accel.setTimeInactivity(10);
+
+  accel.setActivityX(1);
+  accel.setActivityY(1);
+  accel.setActivityZ(1);
+
+  accel.setInactivityX(1);
+  accel.setInactivityY(1);
+  accel.setInactivityZ(1);
+}
+
+void flashLED() {
+  
+   if(ledFlash>blinkRate) {
+  
+    digitalWrite(led, ledState);
+    ledState = !ledState;
+    Serial.println("LED transition");
+    Serial.println(led);
+    ledFlash = 0;
+    
+  }
 }
 
 
